a4ce3154f1e3fafd594172b179c4b4a24727986e
dstyle0210 <dstyle0210@gmail.com>
2022-07-04 15:36:06 +0900

    firebase 셋팅

--- src/router.ts
+++ src/router.ts
@@ -5,7 +5,7 @@ const routes = [
   {
     path: "/",
     name: "Home",
-    component: () => import("./page/index.vue"),
+    component: () => import("./page/home.vue"),
   },
   {
     path: "/test",
