0764d17f75f34bf6b66fef2c3639f375345d882b
dstyle0210 <dstyle0210@gmail.com>
2022-07-05 16:40:29 +0900

    크롤링 추가중..

--- _crawler/start.js
+++ _crawler/start.js
@@ -5,13 +5,17 @@ var browser,page;
 
 const rim = require("./getRIM"); // RIM 데이터 추출
 
+
 (async () => {
     browser = await chromium.launch({
         headless:false
     });
     page = await browser.newPage();
 
-    await rim(page);
+    var codeList = fs.readFileSync("./_crawler/codeList.txt"); // 가져올 종목코드
+    codeList = codeList.toString().split("\r\n");
+
+    await rim(page,codeList);
 
     await browser.close();
 })();
